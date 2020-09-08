debda4e4de213142c8c0d65a169649bba235b57e
Vuqar <vuqarss@hotmail.com>
2020-09-08 19:02:32 +0400

    Removed debugging code (sys.out.println())

--- EnigmaStarter.java
+++ EnigmaStarter.java
@@ -8,6 +8,7 @@ public class EnigmaStarter{
     List<String> reflectorBList;
     int[] generalRotorPositions;
     StringBuilder decoded;
+    boolean turnover=false; //Second rotor special ! If middle rotor is in turnover pos then when first rotor clicks once second rotor clicks once to 
     
 
     EnigmaStarter(List<Rotor> rotorList,String rotorStartPosition,String sentence){
@@ -33,32 +34,21 @@ public void decode(){ //start
 
     for(int i=0;i<sentence.length();i++){ //Started to "press" letters 
         String letter=String.valueOf(sentence.charAt(i));
-        System.out.println("Letter in "+letter);
-        System.out.println("Current rotor config:"+generalRotorPositions[0]+" "+generalRotorPositions[1]+" "+generalRotorPositions[2]+" Confuguration:"+rotorList.get(0).rotorConfig);
         click(0);
         for(int a=0;a<rotorList.size();a++){ //going through rotors{0,1,2}
             letter=forwardFind(rotorList.get(a),letter,a);
         }
-        System.out.println("********** Letter -> Reflect : "+letter);
         letter=reflectorfind(letter);
         for(int a=2;a>-1;a--){ //going through rotors reverse {2,1,0}
             letter=reverseFind(rotorList.get(a),letter,a);
         }
-        System.out.println("********** Letter -> Out : "+letter);
-        System.out.println("Current rotor config:"+generalRotorPositions[0]+" "+generalRotorPositions[1]+" "+generalRotorPositions[2]+" Confuguration:"+rotorList.get(0).rotorConfig);
         decoded.append(letter);
 }
-System.out.println("DECODED : "+decoded.toString()+" -----------------------------------------");
-System.out.println("DECODED  :"+sentence.toString()+" -----------------------------------------");
-System.out.println("Current rotor config:"+generalRotorPositions[0]+" "+generalRotorPositions[1]+" "+generalRotorPositions[2]+" Confuguration:"+rotorList.get(0).rotorConfig);
-
-
-
-
-
+System.out.println("Ciphered Sentence : "+decoded.toString()+" -----------------------------------------");
+System.out.println("Original Sentence : "+sentence.toString()+" -----------------------------------------");
 }//End
+
 public int giveCircularIndex(int index,int size){ //array size = 26 input=26,27,-1 ouput=0,1,25
-    //int size=26;
     if(index<0){
         index=size+index;
     }else if(index>size){
@@ -67,36 +57,32 @@ public int giveCircularIndex(int index,int size){ //array size = 26 input=26,27,
         index=0;
     }
     return index;
-
 }
-public String reverseFind(Rotor rotor,String letter,int a){
 
-    int position= rotor.rotorConfig.indexOf(letter);//letter = H
-    //int positionOfCode=alphabetList.indexOf(rotor.rotorConfig.get(position))-generalRotorPositions[a];//finding next letter to send next rotor
+public String reverseFind(Rotor rotor,String letter,int a){ //finding letter from last rotor -> input(alphabet)
+
+    int position= rotor.rotorConfig.indexOf(letter);
     String entryLetter;
     if(a==0){
          entryLetter=alphabetList.get(position);
     }else{
          entryLetter=alphabetList.get(giveCircularIndex(position+generalRotorPositions[a-1],26)); //D
     }
-    
     System.out.println("Position : "+position+" entryLetter :"+entryLetter+"Rotor number"+a);
     return entryLetter;
 }
-public String reflectorfind(String letter){ //-1
+
+public String reflectorfind(String letter){
     int alphaposition=alphabetList.indexOf(letter);
     String reflected= reflectorBList.get(alphaposition);
     reflected=alphabetList.get(giveCircularIndex(alphabetList.indexOf(reflected)+generalRotorPositions[generalRotorPositions.length-1],26));
-    
-    System.out.println("Reflected : "+reflected);
     return reflected;
 }
-public String forwardFind(Rotor rotor,String letter,int a){
 
+public String forwardFind(Rotor rotor,String letter,int a){ //finding letter from input to last rotor
     int position=alphabetList.indexOf(String.valueOf(letter)); 
     int positionOfCode=alphabetList.indexOf(rotor.rotorConfig.get(position))-generalRotorPositions[a];//finding next letter to send next rotor 
     String codeLetter=alphabetList.get(giveCircularIndex(positionOfCode,26)); //J
-    System.out.println("Position : "+position+" positionOfCode :"+positionOfCode+" CodeLetter :"+codeLetter);
     return codeLetter;
 }
 
@@ -105,20 +91,18 @@ public void generalRotorPositionConfuguration(){
         String position= String.valueOf(rotorStartPosition.charAt(i));
         int rotorposition= alphabetList.indexOf(position);
         if(i+1==2){
-            if(position.equals("E")){// !!! Second rotor special here
+            if(position.equals("E")){// !!! Second rotor special here 
                 turnover=true;
             }
         }
         generalRotorPositions[i]=rotorposition;
 }
-System.out.println("Rotor config is: "+generalRotorPositions[0]+" "+generalRotorPositions[1]+" "+generalRotorPositions[2]);
 }
 
 public void rotorConfiguration(){  // Changing rotorConfig param inside rotor object according to rotor start position ("AAA")
     for(int i=0;i<rotorList.size();i++){
         Rotor rotor = rotorList.get(i);   // 0 = 1 Rotor
         rotor.rotorConfig=getPositionedArray(rotor.rotorConfig,String.valueOf(rotorStartPosition.charAt(i)));
-        System.out.println("Rotor number :"+rotor.rotorNumber+" Rotor Config is "+rotor.rotorConfig);
     }
 }//End
 
@@ -134,63 +118,44 @@ for(int i=0;i<posAlpha;i++){
 }
 return code;
 }//End
-boolean turnover=false;
-public void click(int currentRotorNumber){ //Handles all possible gear actions. Needed to call after reading each letter.
+
+
+public void click(int currentRotorNumber){                                         //Handles all possible gear actions. Needed to call after reading each letter.
     System.out.println("Current__"+generalRotorPositions[currentRotorNumber]);
     int currentRealRNumber=rotorList.get(currentRotorNumber).rotorNumber;
-    System.out.println("------------------------------------------------------------------------------------------------------------"+currentRealRNumber);
-       generalRotorPositions[currentRotorNumber]=generalRotorPositions[currentRotorNumber]+1; // {1,0,0}
+    generalRotorPositions[currentRotorNumber]=generalRotorPositions[currentRotorNumber]+1;
 
-       if(generalRotorPositions[currentRotorNumber]==26){   //Current rotor done one turn IMDEX !!!
-           generalRotorPositions[currentRotorNumber]=0; //Resetting current rotor position and after clicking nect rotor
-       
-        }
-        
+    if(generalRotorPositions[currentRotorNumber]==26){generalRotorPositions[currentRotorNumber]=0;}            //Current rotor done one turn soo needs to reset position to 0
+                                    
         
         if(currentRotorNumber+1==1){
             if(turnover){
                 click(currentRotorNumber+1);
                 turnover=false;
             }
-            if(generalRotorPositions[currentRotorNumber]==17){   //Current rotor done one turn IMDEX !!!
-                
-                     click(currentRotorNumber+1);
-                     
-                     System.out.println("SEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE !"+generalRotorPositions[currentRotorNumber]);
-                
-                
-             }
-        } if(currentRotorNumber+1==2){
+            if(generalRotorPositions[currentRotorNumber]==17){click(currentRotorNumber+1);}
+        }
+
+        if(currentRotorNumber+1==2){
             if(generalRotorPositions[currentRotorNumber]==4){
                 turnover=true;
                 System.out.println("Turnover /*/*/*/*/*/*/*/ "+alphabetList.get(generalRotorPositions[currentRotorNumber]));
             }
-           // System.out.println("ENTERED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+generalRotorPositions[currentRotorNumber]);
-            if(generalRotorPositions[currentRotorNumber]==5){   //Current rotor done one turn IMDEX !!!
-               
+            if(generalRotorPositions[currentRotorNumber]==5){   
                      click(currentRotorNumber+1);
-                     System.out.println("ENTERED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"+generalRotorPositions[currentRotorNumber]);
-                
-                
-             }
-
-        } if(currentRotorNumber+1==3){
+                 }
+            } 
+        if(currentRotorNumber+1==3){
             if(generalRotorPositions[currentRotorNumber]==21){   //Current rotor done one turn IMDEX !!!
-               
-                
-                // click(currentRotorNumber+1);  Third rotor special !! need to config here right way if want to change rotor places randomly
-                
-           
-           
+                     // click(currentRotorNumber+1);  Third rotor special !! need to config here right way if want to change rotor places randomly
+                }
         }
-        }
+
+        // Below confuguring code list according to rotor position 
        List<String> listA =rotorList.get(currentRotorNumber).rotorConfig;
        String temp=listA.get(0);
        listA.remove(0);
        listA.add(temp);
        rotorList.get(currentRotorNumber).rotorConfig=listA; // Setting new code config after click
     }
-
-
-
 }
